af0551e4acbb388f68adc8a3d9456913ae935124
user only <user only@DESKTOP-AEGLGP4>
2018-11-14 10:33:25 +0700

    update game2, hapus dan tambah bola

---
+++ Game/src/model/Tempat.java
@@ -0,0 +1,111 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.awt.Color;
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author user only
+ */
+public class Tempat {
+    private int tinggi; // tinggi tempat Game
+    private int lebar;  // lebar tempat Game
+    private ArrayList<Sel> daftarSel; // daftar sel
+
+    private String isi; // isi file konfigurasi
+    
+    public static int batasKanan;
+    public static int batasBawah;
+    
+    public Tempat() {
+        daftarSel = new ArrayList<Sel>();
+    }
+    
+    
+    /**
+     * Fungsi pembaca file konfigurasi.
+     * Hasil pembacaan file akan disimpan di atribut 'isi' dan juga di atribut daftarSel
+     * @param file 
+     */
+    public void bacaKonfigurasi(File file){
+        
+    }
+    /**
+     * Fungsi penambah daftar sel.
+     * @param sel 
+     */
+    public void tambahSel(Sel sel){
+        daftarSel.add(sel);
+    }
+
+    /**
+     * Fungsi hapus sel.
+     * Sel yang paling akhir diremove dari daftar sel.
+     */
+    public void hapusSel(){
+        if(!daftarSel.isEmpty()){
+            daftarSel.remove(daftarSel.size()-1);
+        }
+    }
+    /**
+     * @return the tinggi
+     */
+    public int getTinggi() {
+        return tinggi;
+    }
+
+    /**
+     * @param tinggi the tinggi to set
+     */
+    public void setTinggi(int tinggi) {
+        this.tinggi = tinggi;
+    }
+
+    /**
+     * @return the lebar
+     */
+    public int getLebar() {
+        return lebar;
+    }
+
+    /**
+     * @param lebar the lebar to set
+     */
+    public void setLebar(int lebar) {
+        this.lebar = lebar;
+    }
+
+    /**
+     * @return the daftarSel
+     */
+    public ArrayList<Sel> getDaftarSel() {
+        return daftarSel;
+    }
+
+    /**
+     * @param daftarSel the daftarSel to set
+     */
+    public void setDaftarSel(ArrayList<Sel> daftarSel) {
+        this.daftarSel = daftarSel;
+    }
+
+    /**
+     * @return the isi
+     */
+    public String getIsi() {
+        return isi;
+    }
+
+    /**
+     * @param isi the isi to set
+     */
+    public void setIsi(String isi) {
+        this.isi = isi;
+    }
+}
---
+++ Game/src/view/GameFrame.java
@@ -0,0 +1,172 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import model.Sel;
+
+/**
+ *
+ * @author user only
+ */
+public class GameFrame extends JFrame {
+    private TempatPanel tempatPanel;
+
+    private JLabel perintahlabel;
+    private JTextField perintahText;
+    private JButton pindahKananButton;
+    private JButton pindahKiriButton;
+    private JButton tambahButton;
+    private JButton hapusButton;
+    
+    private JMenuBar menuBar;
+    private JMenu gameMenu;
+    private JMenuItem exitMenuItem;
+    private JMenuItem bacaKonfigurasiMenuItem;
+
+    public GameFrame(String title) {
+        this.setTitle(title);
+        this.init();
+    }
+
+    public GameFrame(String title, TempatPanel tempatPanel) {
+        setTitle(title);
+        this.tempatPanel = tempatPanel;
+        this.init();
+    }
+
+    public void init() {
+        // set ukuran dan layout
+        this.setSize(800, 500);
+        this.setLayout(new BorderLayout());
+
+        // set menu Bar
+        menuBar = new JMenuBar();
+        gameMenu = new JMenu("Game");
+        exitMenuItem = new JMenuItem("Keluar");
+        bacaKonfigurasiMenuItem = new JMenuItem("Baca");
+        gameMenu.add(bacaKonfigurasiMenuItem);
+        gameMenu.add(exitMenuItem);
+        menuBar.add(gameMenu);
+        setJMenuBar(menuBar);
+
+        //action perform for exitMenuItem
+        exitMenuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        }
+        );
+
+        // panel selatan
+        JPanel southPanel = new JPanel();
+        southPanel.setLayout(new FlowLayout());
+
+        this.perintahlabel = new JLabel("Perintah");
+        southPanel.add(perintahlabel);
+
+        this.perintahText = new JTextField(20);
+        southPanel.add(perintahText);
+
+        this.pindahKananButton = new JButton("Kanan");
+        southPanel.add(pindahKananButton);
+        
+        pindahKananButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                pindahKanan();
+            }
+        });
+        
+        this.pindahKiriButton = new JButton("Kiri");
+        southPanel.add(pindahKiriButton);
+        
+        this.tambahButton = new JButton("tambahBola");
+        southPanel.add(tambahButton);
+        tambahButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                tambahBola();
+                }
+        });
+        
+        this.hapusButton = new JButton("hapusBola");
+        southPanel.add(hapusButton);
+        
+        hapusButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                hapusBola();
+            }
+        });
+        // set contentPane
+        Container cp = this.getContentPane();
+        if (tempatPanel != null) {
+            cp.add(getTempatPanel(), BorderLayout.CENTER);
+        }
+        cp.add(southPanel, BorderLayout.SOUTH);
+
+        // set visible= true
+        this.setVisible(true);
+    }
+
+    /**
+     * Fungsi untuk tambahBola
+     */
+    public void tambahBola(){
+        tempatPanel.getTempat().tambahSel(new Sel(0,0,25,25,'#',Color.BLUE));
+    }
+    
+    /**
+     * Fungsi hapus bola
+     */
+    public void hapusBola(){
+        tempatPanel.getTempat().hapusSel();
+    }
+    /**
+     * Fungsi untuk memindahkan sel dan menggambar ulang
+     */
+    public void pindahKanan() {
+        // posisiX seluruh sel ditambah 20
+        // sehingga sel akan terlihat bergerak ke kanan
+        for (int i = 0; i < getTempatPanel().getTempat().getDaftarSel().size(); i++) {
+            // set posisiX yang baru
+            getTempatPanel().getTempat().getDaftarSel().get(i).geserKanan();
+        }
+        // gambar ulang tempat Panel
+        getTempatPanel().repaint();
+    }
+
+    /**
+     * @return the tempatPanel
+     */
+    public TempatPanel getTempatPanel() {
+        return tempatPanel;
+    }
+
+    /**
+     * @param tempatPanel the tempatPanel to set
+     */
+    public void setTempatPanel(TempatPanel tempatPanel) {
+        this.tempatPanel = tempatPanel;
+    }
+
+}

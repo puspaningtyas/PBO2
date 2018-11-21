775b6175c2129d051c3240fa89a2d34339048f39
puspaningtyas <admin@dosenb>
2018-11-21 11:41:59 +0700

    koneksi ke oracle

---
+++ Oracle/src/model_HR/Regions.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model_HR;
+
+/**
+ *
+ * @author admin
+ */
+public class Regions {
+    private int region_id;
+    private String region_name;
+
+    public Regions() {
+    }
+
+    public Regions(int region_id, String region_name) {
+        this.region_id = region_id;
+        this.region_name = region_name;
+    }
+
+    
+    /**
+     * @return the region_id
+     */
+    public int getRegion_id() {
+        return region_id;
+    }
+
+    /**
+     * @param region_id the region_id to set
+     */
+    public void setRegion_id(int region_id) {
+        this.region_id = region_id;
+    }
+
+    /**
+     * @return the region_name
+     */
+    public String getRegion_name() {
+        return region_name;
+    }
+
+    /**
+     * @param region_name the region_name to set
+     */
+    public void setRegion_name(String region_name) {
+        this.region_name = region_name;
+    }
+    
+    
+}
--- Oracle/src/testOracle/TestOracle2.java
+++ Oracle/src/testOracle/TestOracle2.java
@@ -12,7 +12,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import model.MyOracle;
+import util.MyOracle;
 
 /**
  *
